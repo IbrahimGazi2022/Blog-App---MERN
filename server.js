e92e7d29ff5fb7583076e65a02da6d07565bd905
IbrahimGazi_2022~ <coder.ibrahimgazi@gmail.com>
2023-12-27 04:36:25 +0600

    Setup - Server + Package

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const colors = require('colors');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+// env configuration
+dotenv.config();
+
+// rest object
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+// routes
+app.get('/', (req, res) => {
+    res.status(200).send({
+        'message': "Node Server"
+    });
+});
+
+// Port 
+const PORT = process.env.PORT || 8080;
+
+// Listen
+app.listen(PORT, () => {
+    console.log(`Server Running on ${process.env.DEV_MODE} port ${PORT}`.bgMagenta.black);
+});
