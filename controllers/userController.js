40c17bc59c65a2873ee364685248d8170f909e96
IbrahimGazi_2022~ <coder.ibrahimgazi@gmail.com>
2024-01-02 22:12:52 +0600

    Adding password hashing function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,73 @@
-// get all user
-exports.getAllUsers = (req, res) => {
-    res.status(200).send({
-        message: "Test Route"
-    });
-};
+const userModel = require('../models/userModel');
+const bcrypt = require('bcrypt');
 
 // create new Register user
-exports.registerController = () => { };
+exports.registerController = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        // Check blank data
+        if (!username || !email || !password) {
+            return res.status(400).send({
+                success: false,
+                message: "Please fill all fields"
+            });
+        }
+
+        // exisiting user or not
+        const exisitingUser = await userModel.findOne({ email });
+        if (exisitingUser) {
+            return res.status(401).send({
+                success: false,
+                message: "User already created"
+            });
+        }
+
+        // Password Hashded
+        const hashdedPassword = await bcrypt.hash(password, 10);
+
+
+        // save user data to database
+        const user = new userModel({ username, email, password: hashdedPassword });
+        await user.save();
+        return res.status(200).send({
+            success: true,
+            message: 'Successfully New User created',
+            user
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            success: false,
+            message: 'Error in register callback',
+            error
+        });
+    }
+};
+
+
+
+// get all user
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await userModel.find({});
+        return res.status(200).send({
+            userCount: users.length,
+            success: true,
+            message: "all users data",
+            users
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            success: false,
+            message: 'Error in get all user',
+            error
+        });
+    }
+};
 
 // Login
 exports.loginController = () => { };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const { getAllUsers, loginController, registerController } = require('../controllers/userController');
 
-
 // Router Object
 const router = express.Router();
+
+// REGISTER USER || POST
+router.post('/register', registerController);
+
 
 // GET ALL USER || GET
 router.get('/all-users', getAllUsers);
 
-// REGISTER USER || POST
-router.post('/register', registerController);
 
 // LOGIN USER || POST
 router.post('/login', loginController);
