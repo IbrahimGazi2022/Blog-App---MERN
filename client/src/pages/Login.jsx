bdfce48d6c0d765832aa7b4b11c157a60dd113ea
IbrahimGazi2022 <coder.ibrahimgazi.com>
2024-01-12 01:08:05 +0600

    Login + Register Page design complete

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { Box, Typography, TextField, Button } from "@mui/material";
 
 const Login = () => {
   return (
-    <div>Login Page</div>
-  )
-}
+    <>
+      <Box
+        maxWidth={450}
+        display="flex"
+        flexDirection={"column"}
+        alignItems="center"
+        justifyContent={"center"}
+        margin="auto"
+        marginTop={5}
+        boxShadow="10px 10px 20px #ccc"
+        padding={3}
+        borderRadius={5}
+      >
+        <Typography
+          variant="h4"
+          sx={{ textTransform: "uppercase" }}
+          padding={3}
+          textAlign="center"
+        >
+          Login
+        </Typography>
+        <TextField
+          placeholder="Email"
+          name="email"
+          margin="normal"
+          type={"email"}
+          required
+        />
+        <TextField
+          placeholder="Password"
+          name="password"
+          margin="normal"
+          type={"password"}
+          required
+        />
+        <Button
+          type="submit"
+          sx={{ borderRadius: 3, marginTop: 3 }}
+          variant="contained"
+          color="primary"
+        >
+          Submit
+        </Button>
+        <Button sx={{ borderRadius: 3, marginTop: 3 }}>
+          Not a User ? Please Register
+        </Button>
+      </Box>
+    </>
+  );
+};
 
-export default Login
+export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import { Box, Typography, TextField, Button } from "@mui/material";
 
 const Register = () => {
   return (
-    <div>Register Page</div>
-  )
-}
+    <>
+      <Box
+        maxWidth={450}
+        display="flex"
+        flexDirection={"column"}
+        alignItems="center"
+        justifyContent={"center"}
+        margin="auto"
+        marginTop={5}
+        boxShadow="10px 10px 20px #ccc"
+        padding={3}
+        borderRadius={5}
+      >
+        <Typography
+          variant="h4"
+          sx={{ textTransform: "uppercase" }}
+          padding={3}
+          textAlign="center"
+        >
+          Register
+        </Typography>
+        <TextField
+          placeholder="Name"
+          name="name"
+          margin="normal"
+          type={"text"}
+          required
+        />
+        <TextField
+          placeholder="Email"
+          name="email"
+          margin="normal"
+          type={"email"}
+          required
+        />
+        <TextField
+          placeholder="Password"
+          name="password"
+          margin="normal"
+          type={"password"}
+          required
+        />
+        <Button
+          type="submit"
+          sx={{ borderRadius: 3, marginTop: 3 }}
+          variant="contained"
+          color="primary"
+        >
+          Submit
+        </Button>
+        <Button sx={{ borderRadius: 3, marginTop: 3 }}>
+          Already Registerd ? Please Login
+        </Button>
+      </Box>
+    </>
+  );
+};
 
-export default Register
+export default Register;
